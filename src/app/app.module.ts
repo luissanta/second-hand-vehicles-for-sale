f559fd8f339d30ef15beb2cc5977f1f9d7e06773
Luis <luisfernandosantaospina@gmail.com>
2022-11-04 12:13:49 -0500

    Add list and model for vehicle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehicleModule } from "./vehicle/vehicle.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VehicleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/vehicle/vehicle.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
+
+@NgModule({
+  declarations: [
+    VehicleListComponent
+  ],
+  exports: [
+    VehicleListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VehicleModule { }
